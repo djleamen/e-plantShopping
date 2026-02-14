3f8ac8dc5fe4e3f98d237bc44e597c38570e3f10
DJ Leamen <dilara_leamen@icloud.com>
2026-02-13 23:10:57 -0500

    Add comments to document Redux Provider setup in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// Import Provider component to make Redux store available to all components
 import { Provider } from 'react-redux'
+// Import the Redux store that holds the application state
 import store from './store.js'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Wrap App with Provider to enable Redux store access throughout the app */}
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
 )
